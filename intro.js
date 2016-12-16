80a0d06b236a7879a51949245d79666b85251b69
Bayless <bsmithsalzberg@stuy.edu>
2016-12-15 19:13:13 -0500

    finished

--- intro.js
+++ intro.js
@@ -1,31 +1,38 @@
 var items = document.getElementsByTagName("li");
 
 var addListElement = function(e) {
-	//Hint: You should do this last
-}
+    var thelist = document.getElementById("thelist");
+    var newitem = document.createElement("li");
+    var num = items.length;
+    newitem.innerHTML = "item " + num;
+    thelist.appendChild(newitem);
+    
+};
+
 document.getElementById("b").addEventListener("click",addListElement);
 
 
 var removeListElement = function(e) {
+    this.outerHTML = "";
+    delete this;
+};
 
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click",removeListElement);
 }
-
-
-
-
 
 var defHeading = document.getElementById("h").innerHTML;
 
 var hoverChangeHeading = function(e) {
-	document.getElementById("h").innerHTML = this.innerHTML;
+    document.getElementById("h").innerHTML = this.innerHTML;
 }
 for (var i = 0; i < items.length; i++) {
-	items[i].addEventListener("mouseover",hoverChangeHeading);
+    items[i].addEventListener("mouseover",hoverChangeHeading);
 }
 
 var resetHeading = function(e) {
-	document.getElementById("h").innerHTML = defHeading;
+    document.getElementById("h").innerHTML = defHeading;
 }
 for (var i = 0; i < items.length; i++) {
-	items[i].addEventListener("mouseout",resetHeading);
+    items[i].addEventListener("mouseout",resetHeading);
 }
